92197d6bf45101d12643d2d8d37db98c0de786e8
Javier López Úbeda <jlopezcur@gmail.com>
2022-07-22 01:15:03 +0200

    feat(telescope): added resume action

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -34,23 +34,25 @@ require "telescope".load_extension "fzy_native"
 -- Mappings
 --
 
+vim.keymap.set("n", "<leader>fr", require "telescope.builtin".resume, {desc = "Telescope: Resume"})
+
 -- Files
-vim.keymap.set("n", "<leader>ff", require "telescope.builtin".find_files)
-vim.keymap.set("n", "<leader>fg", require "telescope.builtin".live_grep)
-vim.keymap.set("n", "<leader>fb", require "telescope.builtin".buffers)
-vim.keymap.set("n", "<leader>fh", require "telescope.builtin".help_tags)
+vim.keymap.set("n", "<leader>ff", require "telescope.builtin".find_files, {desc = "Telescope: Find files"})
+vim.keymap.set("n", "<leader>fg", require "telescope.builtin".live_grep, {desc = "Telescope: Live grep"})
+vim.keymap.set("n", "<leader>fb", require "telescope.builtin".buffers, {desc = "Telescope: Buffers"})
+vim.keymap.set("n", "<leader>fh", require "telescope.builtin".help_tags, {desc = "Telescope: Help tags"})
 
 -- LSP
-vim.keymap.set("n", "gr", require "telescope.builtin".lsp_references)
+vim.keymap.set("n", "gr", require "telescope.builtin".lsp_references, {desc = "Telescope: LSP References"})
 
 -- Git
 -- Remember: Ctrl-a create a new brnach, Ctrl-d remove selected branch
-vim.keymap.set("n", "<leader>gb", require "telescope.builtin".git_branches)
-vim.keymap.set("n", "<leader>gc", require "telescope.builtin".git_bcommits)
-vim.keymap.set("n", "<leader>gt", require "telescope.builtin".git_stash)
+vim.keymap.set("n", "<leader>gb", require "telescope.builtin".git_branches, {desc = "Telescope: Git branches"})
+vim.keymap.set("n", "<leader>gc", require "telescope.builtin".git_bcommits, {desc = "Telescope: Git bcommits"})
+vim.keymap.set("n", "<leader>gt", require "telescope.builtin".git_stash, {desc = "Telescope: Git stash"})
 
 -- Repo List
-vim.keymap.set("n", "<leader>rl", ":Telescope repo list<CR>")
+vim.keymap.set("n", "<leader>rl", ":Telescope repo list<CR>", {desc = "Telescope: Repo list"})
 
 -- Neoclip
-vim.keymap.set("n", "<leader>yl", ":Telescope neoclip<CR>")
+vim.keymap.set("n", "<leader>yl", ":Telescope neoclip<CR>", {desc = "Telescope: Neoclip"})
