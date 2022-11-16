abe30ef3e970f661fc2d92aa7e3eb179272a9276
Javier López Úbeda <jlopezcur@gmail.com>
2022-11-16 02:01:26 +0100

    feat(telescope): simplified file

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,9 +2,11 @@
 -- configuration
 --
 
+local telescope = require("telescope")
 local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
 
-require("telescope").setup {
+telescope.setup {
   defaults = {
     prompt_prefix = " ﮊ ",
     mappings = {
@@ -31,9 +33,9 @@ require("telescope").setup {
   }
 }
 
-require "telescope".load_extension "neoclip"
-require "telescope".load_extension "fzy_native"
-require "telescope".load_extension "ui-select"
+telescope.load_extension "neoclip"
+telescope.load_extension "fzy_native"
+telescope.load_extension "ui-select"
 
 --
 -- keymaps
@@ -41,22 +43,22 @@ require "telescope".load_extension "ui-select"
 
 -- Remember: C-q create a quickfix list with the results
 
-vim.keymap.set("n", "<leader>fr", require "telescope.builtin".resume, {desc = "Telescope: Resume"})
+vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Telescope: Resume" })
 
 -- Files
-vim.keymap.set("n", "<leader>ff", require "telescope.builtin".find_files, {desc = "Telescope: Find files"})
-vim.keymap.set("n", "<leader>fg", require "telescope.builtin".live_grep, {desc = "Telescope: Live grep"})
-vim.keymap.set("n", "<leader>fb", require "telescope.builtin".buffers, {desc = "Telescope: Buffers"})
-vim.keymap.set("n", "<leader>fh", require "telescope.builtin".help_tags, {desc = "Telescope: Help tags"})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
 
 -- LSP
-vim.keymap.set("n", "gr", require "telescope.builtin".lsp_references, {desc = "Telescope: LSP References"})
+vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Telescope: LSP References" })
 
 -- Git
 -- Remember: Ctrl-a create a new brnach, Ctrl-d remove selected branch
-vim.keymap.set("n", "<leader>gb", require "telescope.builtin".git_branches, {desc = "Telescope: Git branches"})
-vim.keymap.set("n", "<leader>gc", require "telescope.builtin".git_bcommits, {desc = "Telescope: Git bcommits"})
-vim.keymap.set("n", "<leader>gt", require "telescope.builtin".git_stash, {desc = "Telescope: Git stash"})
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Telescope: Git branches" })
+vim.keymap.set("n", "<leader>gc", builtin.git_bcommits, { desc = "Telescope: Git bcommits" })
+vim.keymap.set("n", "<leader>gt", builtin.git_stash, { desc = "Telescope: Git stash" })
 
 -- Neoclip
-vim.keymap.set("n", "<leader>yl", ":Telescope neoclip<CR>", {desc = "Telescope: Neoclip"})
+vim.keymap.set("n", "<leader>yl", ":Telescope neoclip<CR>", { desc = "Telescope: Neoclip" })
