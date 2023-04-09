3caf0532d1abfaeb870592a66ecb430c959f822c
Javier López Úbeda <jlopezcur@gmail.com>
2023-04-09 23:04:23 +0200

    chore(theme): use the tokyonight lua syntax

--- nvim/.config/nvim/after/plugin/color.lua
+++ nvim/.config/nvim/after/plugin/color.lua
@@ -5,4 +5,4 @@
 vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_transparent = false
-vim.cmd("colorscheme tokyonight")
+vim.cmd [[colorscheme tokyonight]]
