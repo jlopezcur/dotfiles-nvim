bc72e530f440b4ce93d4a8f1e216715586f1aa35
Javier López Úbeda <jlopezcur@gmail.com>
2023-07-19 02:13:27 +0200

    feat(toggleterm): added key for Lazy initialization

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,29 +1,35 @@
 return {
-  "akinsho/nvim-toggleterm.lua",
-  event = "VeryLazy",
-  config = function()
-    local colors = require("tokyonight.colors").setup()
-    require "toggleterm".setup {
-      size = 20,
-      open_mapping = [[<c-\>]],
-      -- shade_filetypes = {},
-      -- shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-      start_in_insert = true,
-      persist_size = true,
-      direction = "float", -- vertical | horizontal | float,
-      float_opts = {
-        border = "single",
-        winBlend = 1
-      },
-      shade_terminals = false,
-      highlights = {
-        Normal = {
-          guibg = colors.bg_dark
-        }
-      }
-    }
+	'akinsho/nvim-toggleterm.lua',
+	config = function()
+		local colors = require('tokyonight.colors').setup()
+		require('toggleterm').setup({
+			size = 20,
+			open_mapping = [[<c-\>]],
+			-- shade_filetypes = {},
+			-- shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+			start_in_insert = true,
+			persist_size = true,
+			direction = 'float', -- vertical | horizontal | float,
+			float_opts = {
+				border = 'single',
+				winBlend = 1,
+			},
+			shade_terminals = false,
+			highlights = {
+				Normal = {
+					guibg = colors.bg_dark,
+				},
+			},
+		})
 
-    -- keymaps
-    vim.keymap.set("t", "<Leader><Esc>", "<C-\\><C-n>", { desc = "Change to normal mode" })
-  end
+		-- keymaps
+		vim.keymap.set('t', '<Leader><Esc>', '<C-\\><C-n>', { desc = 'Change to normal mode' })
+	end,
+	keys = {
+		{
+			'<c-\\>',
+			'<Cmd>exe v:count1 . "ToggleTerm"<CR>',
+			desc = 'Open terminal',
+		},
+	},
 }
