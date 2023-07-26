3d238fa99e4f8d66e1c25b73e05dd5fabb4f0d07
Javier López Úbeda <jlopezcur@gmail.com>
2023-07-27 00:33:22 +0200

    feat(lazy): upgrade lock file

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -57,6 +57,9 @@ return {
 				'sql',
 				'zig',
 			},
+			sync_install = false,
+			auto_install = true,
+			ignore_install = {},
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
