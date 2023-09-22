e46ae8533e7114e6ef83922ca6f78b8769e6e8f8
Javier López Úbeda <jlopezcur@gmail.com>
2023-09-23 00:31:39 +0200

    feat: change completion C-Space to A-Space for avoid collision

--- nvim/.config/nvim/lua/plugins/completition.lua
+++ nvim/.config/nvim/lua/plugins/completition.lua
@@ -39,7 +39,7 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<A-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(function(fallback)
