974cfc4096053f222d6c6d2d74253dce6459e2e9
Javier López Úbeda <jlopezcur@gmail.com>
2023-10-30 13:46:45 +0100

    feat: improve ibl configuration

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,5 +1,31 @@
 return {
-	'lukas-reineke/indent-blankline.nvim',
-	main = 'ibl',
-	opts = {},
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {
+    indent = {
+      char = '│',
+    },
+    scope = {
+      enabled = false,
+    },
+    exclude = {
+      filetypes = {
+        'alpha',
+        'dashboard',
+        'Trouble',
+        'neo-tree',
+        'help',
+        'terminal',
+        'lazy',
+        'lspinfo',
+        'TelescopePrompt',
+        'TelescopeResults',
+        'mason',
+        '',
+      },
+    },
+    whitespace = {
+      remove_blankline_trail = true,
+    },
+  },
 }
