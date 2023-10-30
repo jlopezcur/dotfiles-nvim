c8145ba0741dd542bcbf495c4a2870515d023341
Javier López Úbeda <jlopezcur@gmail.com>
2023-10-30 13:13:59 +0100

    feat: change formatter.nvim by conform.nvim

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,50 @@
+return {
+  'stevearc/conform.nvim',
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '=',
+      function()
+        require('conform').format({ async = true, lsp_fallback = true })
+      end,
+      mode = '',
+      desc = 'Format buffer',
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      -- Conform will run multiple formatters sequentially
+      python = { 'isort', 'black' },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { { 'prettierd', 'prettier' } },
+      javascriptreact = { { 'prettierd', 'prettier' } },
+      typescript = { { 'prettierd', 'prettier' } },
+      typescriptreact = { { 'prettierd', 'prettier' } },
+      vue = { { 'prettierd', 'prettier' } },
+      css = { { 'prettierd', 'prettier' } },
+      less = { { 'prettierd', 'prettier' } },
+      scss = { { 'prettierd', 'prettier' } },
+      graphql = { { 'prettierd', 'prettier' } },
+      html = { { 'prettierd', 'prettier' } },
+      json = { { 'prettierd', 'prettier' } },
+      yaml = { { 'prettierd', 'prettier' } },
+      latex = { 'latexindent' },
+      markdown = { { 'prettierd', 'prettier' } },
+      rust = { 'rustfmt' },
+      sh = { 'shfmt' },
+      -- Use the "_" filetype to run formatters on filetypes that don't
+      -- have other formatters configured.
+      ['_'] = { 'trim_whitespace' },
+    },
+    -- format_on_save = {
+    --   -- These options will be passed to conform.format()
+    --   timeout_ms = 500,
+    --   lsp_fallback = true,
+    -- },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = 'v:lua.require\'conform\'.formatexpr()'
+  end,
+}
