10edc02ffdaf546926116acbffa847837ac68dd3
Javier López Úbeda <jlopezcur@gmail.com>
2023-10-30 13:38:50 +0100

    feat: completions as cmp plugin file

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,75 @@
+return {
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'saadparwaiz1/cmp_luasnip',
+    'onsails/lspkind-nvim',
+  },
+  config = function()
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local luasnip = require('luasnip')
+    local cmp = require('cmp')
+    local lspkind = require('lspkind')
+
+    local has_words_before = function()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    end
+
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      window = {
+        documentation = {
+          border = 'solid',
+        },
+        completion = {
+          border = 'solid',
+        },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<A-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'path' },
+      }, {
+        { name = 'buffer' },
+      }),
+      formatting = {
+        format = lspkind.cmp_format({ with_text = true, maxwidth = 50 }),
+      },
+    })
+  end,
+}
