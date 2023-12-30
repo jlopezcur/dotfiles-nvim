7656c7a8bafcf2f60a618001888cc6403e66107d
Javier López Úbeda <jlopezcur@gmail.com>
2023-12-30 09:58:14 +0100

    feat: change the tmux plugin and the keybindings

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -1,6 +1,15 @@
 return {
-  'aserowy/tmux.nvim',
-  config = function()
-    require('tmux').setup()
-  end,
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+  },
+  keys = {
+    { '<c-w>h', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<c-w>j', '<cmd>TmuxNavigateDown<cr>' },
+    { '<c-w>k', '<cmd>TmuxNavigateUp<cr>' },
+    { '<c-w>l', '<cmd>TmuxNavigateRight<cr>' },
+  },
 }
