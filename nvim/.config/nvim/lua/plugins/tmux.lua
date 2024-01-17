de0d566c2c02b3498d496f830116c58feae3454e
Javier López Úbeda <jlopezcur@gmail.com>
2024-01-17 23:00:38 +0100

    feat(tmux): better sync with tmux

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -7,9 +7,9 @@ return {
     'TmuxNavigateRight',
   },
   keys = {
-    { '<c-w>h', '<cmd>TmuxNavigateLeft<cr>' },
-    { '<c-w>j', '<cmd>TmuxNavigateDown<cr>' },
-    { '<c-w>k', '<cmd>TmuxNavigateUp<cr>' },
-    { '<c-w>l', '<cmd>TmuxNavigateRight<cr>' },
+    { '<c-w><c-h>', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<c-w><c-j>', '<cmd>TmuxNavigateDown<cr>' },
+    { '<c-w><c-k>', '<cmd>TmuxNavigateUp<cr>' },
+    { '<c-w><c-l>', '<cmd>TmuxNavigateRight<cr>' },
   },
 }
