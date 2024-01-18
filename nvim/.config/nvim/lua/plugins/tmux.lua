f40b28ee4fe7801016c4c8f38e879e3e66dddba7
Javier López Úbeda <jlopezcur@gmail.com>
2024-01-18 22:40:34 +0100

    feat(tmux): added simpler keymaps pane movements

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -11,5 +11,9 @@ return {
     { '<c-w><c-j>', '<cmd>TmuxNavigateDown<cr>' },
     { '<c-w><c-k>', '<cmd>TmuxNavigateUp<cr>' },
     { '<c-w><c-l>', '<cmd>TmuxNavigateRight<cr>' },
+    { '<c-w>h', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<c-w>j', '<cmd>TmuxNavigateDown<cr>' },
+    { '<c-w>k', '<cmd>TmuxNavigateUp<cr>' },
+    { '<c-w>l', '<cmd>TmuxNavigateRight<cr>' },
   },
 }
