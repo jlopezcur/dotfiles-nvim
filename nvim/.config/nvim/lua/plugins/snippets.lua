8d359dd062f491804d310b757976c93a568cc7bf
Javier López Úbeda <jlopezcur@gmail.com>
2024-06-02 00:01:07 +0200

    feat: improve snippets

--- nvim/.config/nvim/lua/plugins/snippets.lua
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -1,15 +1,18 @@
 return {
   {
     'L3MON4D3/LuaSnip',
-    event = 'InsertEnter',
     dependencies = {
       'rafamadriz/friendly-snippets',
     },
+    version = 'v2.*',
+    build = 'make install_jsregexp',
+    event = 'InsertEnter',
     config = function()
       local ls = require('luasnip')
       local s = ls.snippet
       local t = ls.text_node
       local i = ls.insert_node
+      local fmt = require('luasnip.extras.fmt').fmta
       local types = require('luasnip.util.types')
       local rep = require('luasnip.extras').rep
       local snippet = ls.parser.parse_snippet
@@ -36,32 +39,91 @@ return {
         snippet('exp', 'export const ${1:name} = (${2:params}) => $3;'),
         snippet('for', 'for (let ${1:i} = 0; $1 < ${2:length}; $1++) {\n\t$3\n}'),
         snippet('it', 'it(\'${1:test}\', () => {\n\t$2\n});'),
-        s('des', {
-          t('describe(\''),
-          i(1, 'test'),
-          t({
-            '\', () => {',
-            '\tconst cases = [',
-            '\t\t[\'Case 1\', {}, []],',
-            '\t\t[\'Case 2\', {}, []],',
-            '\t];',
-            '',
-            '\tit.each(cases)(\'%s\', (_title, args, expected) => {',
-            '\t\texpect(',
-          }),
-          i(2, 'func'),
-          t({
-            '(args)).toEqual(expected);',
-            '\t});',
-            '});',
-          }),
-        }),
+
+        s(
+          'tve',
+          fmt(
+            [[
+import { describe, test, expect } from 'vitest';
+
+import { <func> } from './<file>';
+
+describe('<func>', () =>> {
+  const cases: [string, null, null][] = [
+    ['Case 1', null, null],
+    ['Case 2', null, null],
+  ];
+
+  test.each(cases)('%s', (_title, args, expected) =>> {
+    expect(<func>(args)).toEqual(expected);
+  });
+});
+          ]],
+            {
+              func = i(1, 'func'),
+              file = i(2, 'file'),
+            },
+            {
+              repeat_duplicates = true,
+            }
+          )
+        ),
+
+        s(
+          'tv',
+          fmt(
+            [[
+import { describe, test, expect } from 'vitest';
+
+import { <func> } from './<file>';
+
+describe('<func>', () =>> {
+  test('<test>', () =>> {
+    expect(<func>(<args>)).toEqual(<expected>);
+  });
+});
+          ]],
+            {
+              func = i(1, 'func'),
+              file = i(2, 'file'),
+              test = i(3, 'file'),
+              args = i(4, 'file'),
+              expected = i(5, 'file'),
+            },
+            {
+              repeat_duplicates = true,
+            }
+          )
+        ),
+
+        -- s('vte', {
+        --   t('import { describe, test, expect } from \'vitest\';'),
+        --   t('.'),
+        --   t('describe(\''),
+        --   i(1, 'test'),
+        --   t({
+        --     '\', () => {',
+        --     '\tconst cases = [',
+        --     '\t\t[\'Case 1\', {}, []],',
+        --     '\t\t[\'Case 2\', {}, []],',
+        --     '\t];',
+        --     '',
+        --     '\ttest.each(cases)(\'%s\', (_title, args, expected) => {',
+        --     '\t\texpect(',
+        --   }),
+        --   i(2, 'func'),
+        --   t({
+        --     '(args)).toEqual(expected);',
+        --     '\t});',
+        --     '});',
+        --   }),
+        -- }),
       })
 
       ls.add_snippets('javascriptreact', {
         s('re', {
           t({
-            'import React from \'react\';',
+            'import * as React from \'react\';',
             'import PropTypes from \'prop-types\';',
             '',
             'export const ',
@@ -83,7 +145,7 @@ return {
       ls.add_snippets('typescriptreact', {
         s('re', {
           t({
-            'import React from \'react\';',
+            'import * as React from \'react\';',
             '',
             'interface Props {',
             '\tparam1: string;',
