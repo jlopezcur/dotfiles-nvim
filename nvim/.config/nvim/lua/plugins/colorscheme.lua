942f6a9a29ca517fab1060f4c607cab846ccb00b
Javier López Úbeda <jlopezcur@gmail.com>
2024-06-02 00:02:52 +0200

    feat: improve colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,28 @@
 return {
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('tokyonight').setup({
-  --       style = 'hight',
-  --       transparent = false, -- Enable this to disable setting the background color
-  --       styles = {
-  --         -- Style to be applied to different syntax groups
-  --         -- Value is any valid attr-list value for `:help nvim_set_hl`
-  --         comments = { italic = true },
-  --         keywords = { italic = true },
-  --         functions = {},
-  --         variables = {},
-  --         -- Background styles. Can be "dark", "transparent" or "normal"
-  --         sidebars = 'dark', -- style for sidebars, see below
-  --         floats = 'dark', -- style for floating windows
-  --       },
-  --       sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  --       hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  --       dim_inactive = false, -- dims inactive windows
-  --       lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-  --     })
-  --
-  --     vim.cmd([[colorscheme tokyonight-night]]) -- storm, night, moon
-  --   end,
-  -- },
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'folke/tokyonight.nvim',
+    lazy = false,
     priority = 1000,
     config = function()
-      require('catppuccin').setup({})
-      vim.cmd.colorscheme('catppuccin')
+      require('tokyonight').setup({})
+      vim.cmd.colorscheme('tokyonight-night') -- storm, night, moon
     end,
   },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   config = function()
+  --     require('catppuccin').setup({})
+  --     vim.cmd.colorscheme('catppuccin')
+  --   end,
+  -- },
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     require('kanagawa').setup({})
+  --     vim.cmd.colorscheme('kanagawa')
+  --   end,
+  -- },
 }
