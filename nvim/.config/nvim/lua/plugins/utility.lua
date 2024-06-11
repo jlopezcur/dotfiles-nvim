1e86cbff700833a539e93a72fc8217403b8958e5
Javier López Úbeda <jlopezcur@gmail.com>
2024-06-12 01:01:25 +0200

    chore: removed default redundant values

--- nvim/.config/nvim/lua/plugins/utility.lua
+++ nvim/.config/nvim/lua/plugins/utility.lua
@@ -3,21 +3,6 @@ return {
     'rcarriga/nvim-notify',
     lazy = false,
     config = function()
-      require('notify').setup({
-        stages = 'slide',
-        render = 'default',
-        timeout = 5000,
-        minimum_width = 50,
-        max_width = 120,
-        icons = {
-          ERROR = '',
-          WARN = '',
-          INFO = '',
-          DEBUG = '',
-          TRACE = '✎',
-        },
-      })
-
       vim.notify = require('notify')
     end,
   },
