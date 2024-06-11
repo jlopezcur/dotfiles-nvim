44dc1cdf5097eb8881a8e5638065147a20d928ba
Javier López Úbeda <jlopezcur@gmail.com>
2024-06-12 01:01:55 +0200

    feat: control scrollbar UI

--- nvim/.config/nvim/lua/plugins/scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/scrollbar.lua
@@ -1,5 +1,17 @@
 return {
   {
     'dstein64/nvim-scrollview',
+    config = function()
+      require('scrollview').setup({
+        excluded_filetypes = { 'neo-tree' },
+        current_only = true,
+        base = 'right',
+        -- signs_on_startup = { 'all' },
+        diagnostics_error_symbol = '󰅚',
+        diagnostics_warn_symbol = '󰀪',
+        diagnostics_hint_symbol = '󰌶',
+        diagnostics_info_symbol = '',
+      })
+    end,
   },
 }
