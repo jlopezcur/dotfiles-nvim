2eafcfc147532995a61e50a1c0b18007bdfa3dd8
Javier López Úbeda <jlopezcur@gmail.com>
2024-11-19 22:40:46 +0100

    feat: upgrade lock file

--- nvim/.config/nvim/lua/plugins/cursor.lua
+++ nvim/.config/nvim/lua/plugins/cursor.lua
@@ -1,5 +1,12 @@
 return {
-  {
-    'RRethy/vim-illuminate',
-  },
+  'RRethy/vim-illuminate',
+  config = function()
+    require('illuminate').configure({
+      providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+      },
+    })
+  end,
 }
