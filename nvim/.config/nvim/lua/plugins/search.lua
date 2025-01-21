9708a3318cc35e0a370d389492ea808225f4e4d7
Javier López Úbeda <jlopezcur@gmail.com>
2025-01-21 23:25:38 +0100

    fix: spectre keymap overlapping

--- nvim/.config/nvim/lua/plugins/search.lua
+++ nvim/.config/nvim/lua/plugins/search.lua
@@ -9,7 +9,7 @@ return {
         ':lua require(\'spectre\').open_visual({ select_word = true })<cr>',
         desc = 'Spectre search current word',
       },
-      { '<leader>s', ':lua require(\'spectre\').open_visual()<cr>', desc = 'Spectre open visual' },
+      { '<leader>sa', ':lua require(\'spectre\').open_visual()<cr>', desc = 'Spectre open visual' },
       { '<leader>sp', ':lua require(\'spectre\').open_file_search()<cr>', desc = 'Spectre search in current file' },
     },
     opts = {},
